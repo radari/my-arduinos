f61cfcec6df881e27d6190133181d743b906f064
sturrion <sturrion@gmail.com>
2015-01-23 21:49:42 +0100

    binary dice with button

--- BinaryDice/BinaryDice.ino
+++ BinaryDice/BinaryDice.ino
@@ -1,20 +1,32 @@
-const unsigned int LED_BIT0 = 12;
+const unsigned int LED_BIT0 = 8;
 const unsigned int LED_BIT1 = 10;
-const unsigned int LED_BIT2 = 8;
+const unsigned int LED_BIT2 = 12;
+
+const unsigned int BUTTON_PIN = 7;
 
 const unsigned int TIME_OFF = 3000; // 3 seconds
 
 void setup() {
+  pinMode(13, OUTPUT);
+  
   pinMode(LED_BIT0, OUTPUT);
   pinMode(LED_BIT1, OUTPUT);
   pinMode(LED_BIT2, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
   
   randomSeed(analogRead(A0));
-  long result = random(1, 7);
-  output_result(result);
 }
 
-void loop(){
+int current_value = 0;
+int old_value = 0;
+
+void loop() {
+  current_value = digitalRead(BUTTON_PIN);
+  if (current_value != old_value && current_value == HIGH) {
+    output_result(random(1, 7));
+    delay(50);
+  }
+  old_value = current_value;
 }
 
 void output_result(const long result) {
