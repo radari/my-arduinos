46111fc8628d00c6bd88c6a22d0db2302ccf1cb4
sturrion <sturrion@gmail.com>
2015-03-23 22:23:12 +0100

    Look up table

---
+++ C_book_examples/CosLUT/CosLUT.ino
@@ -0,0 +1,27 @@
+float cosLUT[(int) (360.0 * 1 / 0.5)];
+const float DEG2RAD = PI / 180;
+
+const float cosinePrecision = 0.5;
+const int cosinePeriod = (int) (360.0 * 1 / cosinePrecision);
+
+
+void setup() {
+  initCosineLUT();
+}
+
+void loop() {
+  // nothing for now!
+}
+
+void initCosineLUT() {
+  for (int i = 0; i < cosinePeriod; i++) {
+    cosLUT[i] = (float) cos(i * DEG2RAD * cosinePrecision);
+  }
+}
+
+float myFastCosine(float angle) {
+  return cosLUT[(int) (angle * 1 / cosinePrecision) % cosinePeriod];
+}
+
+
+
