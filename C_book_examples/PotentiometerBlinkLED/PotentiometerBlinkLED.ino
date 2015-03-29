85e5656720bbbf994178746f1a63563aea1d307c
sturrion <sturrion@gmail.com>
2015-03-29 22:21:34 +0200

    Analog inputs. The potentiometer as delay.

--- C_book_examples/PotentiometerBlinkLED/PotentiometerBlinkLED.ino
+++ C_book_examples/PotentiometerBlinkLED/PotentiometerBlinkLED.ino
@@ -2,9 +2,13 @@ int potPin = 0;           // pin number where the potentiometer is connected
 const int ledPin = 8;     // pin of the digital output related to the LED
 int potValue = 0;         // variable storing the voltage value measured at potPin pin
 
+float voltageValue = 0.;  // variable storing the voltage calculated
+
 void setup() {
   // It's to turn integrated led off. 
   pinMode(13, OUTPUT);
+  
+  Serial.begin(9600);          // Serial commmunication setup at 9600 baud
   
   pinMode(ledPin, OUTPUT);    // define ledPin pin as an output
 }
@@ -16,4 +20,7 @@ void loop() {
   delay(potValue);                  // pause the program during potValue millisecond
   digitalWrite(ledPin, LOW);        // turn off the LED
   delay(potValue);                  // pause the program during potValue millisecond
+  
+  voltageValue = 5. * (potValue / 1023.);  // calculate the voltage
+  Serial.println(voltageValue);            // write the voltage value
 }
